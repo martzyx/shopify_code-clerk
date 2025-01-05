2cc10fa000316aae92fe6f445c6c3c3f3b000132
martz <mkg@clerk.io>
2025-01-05 12:08:49 +0100

    logging

--- code_bot.py
+++ code_bot.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import json
 import pyotp
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -23,8 +24,47 @@ TOTP = pyotp.TOTP(SECRET_KEY)
 # Initialize the Slack app
 app = App(token=SLACK_BOT_TOKEN)
 
+# File to store usage count and user data
+DATA_FILE = "/app/data/user_usage.json"
+
+# Load user usage data from file
+def load_user_data():
+    try:
+        with open(DATA_FILE, "r") as f:
+            data = json.load(f)
+            return data
+    except FileNotFoundError:
+        print("File not found. Initializing user data to an empty dictionary.")
+        return {}
+    except Exception as e:
+        print(f"Error loading user data: {e}")
+        return {}
+
+# Save user usage data to file
+def save_user_data(data):
+    try:
+        with open(DATA_FILE, "w") as f:
+            json.dump(data, f)
+    except Exception as e:
+        print(f"Error saving user data: {e}")
+
+# Initialize user data
+user_data = load_user_data()
+
 @app.message("!shopify")
-def send_2fa(message, say):
+def send_2fa(message, say, logger):
+    global user_data
+
+    # Extract channel ID and user ID from the message
+    channel = message.get("channel")
+    user_id = message.get("user")
+    user_info = app.client.users_info(user=user_id)
+    username = user_info["user"]["name"]
+
+    # Increment the interaction count for the user
+    user_data[username] = user_data.get(username, 0) + 1
+    save_user_data(user_data)
+
     print(f"Received message: {message}")
 
     channel_type = message.get("channel_type")
@@ -41,6 +81,7 @@ def send_2fa(message, say):
 
     code = TOTP.now()
     say(text=f"Your TOTP code is: {code}", channel=dm_channel)
+    logger.info(f"{username} has interacted with the bot {user_data[username]} times.")
     print(datetime.datetime.utcnow().strftime("%H:%M:%S") + f" Sent 2FA code to {user_id}")
 
 # Start the app with Socket Mode
